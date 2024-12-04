7f17c1e18d2806c2b532b7df7313f9d4ee4b97f1
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-05 01:52:44 +0500

    Добавлен путь для sections

--- config/urls.py
+++ config/urls.py
@@ -40,10 +40,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # app urls
+    path('', include('sections.urls', namespace='sections')),
     path('users/', include('users.urls', namespace='users')),
 
     # documentation urls
-    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
