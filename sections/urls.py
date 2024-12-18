59a747bb337109a31d78f3abb8675e43860cba2c
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-19 01:23:58 +0500

    Добавлены маршруты тестов

--- sections/urls.py
+++ sections/urls.py
@@ -4,8 +4,7 @@ from sections.apps import SectionsConfig
 
 from sections.views import SectionListAPIView, SectionCreateAPIView, SectionRetrieveAPIView, SectionUpdateAPIView, \
     SectionDestroyAPIView, ContentListAPIView, ContentCreateAPIView, ContentRetrieveAPIView, ContentUpdateAPIView, \
-    ContentDestroyAPIView
-
+    ContentDestroyAPIView, TestListAPIView, TestQuestionRetriveAPIView
 
 app_name = SectionsConfig.name
 
@@ -17,13 +16,16 @@ urlpatterns = [
     path('section/create/', SectionCreateAPIView.as_view(), name='section_create'),
     path('section/<int:pk>/', SectionRetrieveAPIView.as_view(), name='section_detail'),
     path('section/<int:pk>/update/', SectionUpdateAPIView.as_view(), name='section_update'),
-    path('section/<int:pk>/delete', SectionDestroyAPIView.as_view(), name='section_delete'),
+    path('section/<int:pk>/delete/', SectionDestroyAPIView.as_view(), name='section_delete'),
 
     # SectionContant urlpatterns
     path('content/', ContentListAPIView.as_view(), name='content_list'),
     path('content/create/', ContentCreateAPIView.as_view(), name='content_create'),
     path('content/<int:pk>/', ContentRetrieveAPIView.as_view(), name='content_detail'),
     path('content/<int:pk>/update/', ContentUpdateAPIView.as_view(), name='content_update'),
-    path('content/<int:pk>/delete', ContentDestroyAPIView.as_view(), name='content_delete'),
+    path('content/<int:pk>/delete/', ContentDestroyAPIView.as_view(), name='content_delete'),
 
+    # Tests urlpatterns
+    path('tests/', TestListAPIView.as_view(), name='tests_list'),
+    path('tests/<int:pk>', TestQuestionRetriveAPIView.as_view(), name='test')
 ] + router.urls
